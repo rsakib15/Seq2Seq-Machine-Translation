9e98d626770eaffd4c94713fa697e8af84d277e9
Sakib Rahman <srskb15@gmail.com>
2018-09-13 21:09:31 +0600

    SA | Train updated

--- train.py
+++ train.py
@@ -1,4 +1,7 @@
+import random
 import time
+import math
+import matplotlib.ticker as ticker
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,151 +10,113 @@ from torch import nn, optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 
-from dataset import TextDataset
+from dataset import prepareData,tensorsFromPair
 from model.seq2seq import AttnDecoderRNN, DecoderRNN, EncoderRNN
 
 SOS_token = 0
 EOS_token = 1
 MAX_LENGTH = 10
-lang_dataset = TextDataset()
-lang_dataloader = DataLoader(lang_dataset, shuffle=True)
-input_size = lang_dataset.input_lang_words
 hidden_size = 256
-output_size = lang_dataset.output_lang_words
-total_epoch = 20
-
-print(input_size)
-print(output_size)
-
-encoder = EncoderRNN(input_size, hidden_size)
-decoder = DecoderRNN(hidden_size, output_size, n_layers=2)
-attn_decoder = AttnDecoderRNN(hidden_size, output_size, n_layers=2)
 use_attn = True
+teacher_forcing_ratio = 0.5
+
+input_lang, output_lang, pairs = prepareData('eng', 'ben', False)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
 
 
-if torch.cuda.is_available():
-    encoder = encoder.cuda()
-    decoder = decoder.cuda()
-    attn_decoder = attn_decoder.cuda()
+def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length=MAX_LENGTH):
+    encoder_hidden = encoder.initHidden()
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    input_length = input_tensor.size(0)
+    target_length = target_tensor.size(0)
+
+    encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+    loss = 0
+
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(input_tensor[ei], encoder_hidden)
+        encoder_outputs[ei] = encoder_output[0, 0]
+
+    decoder_input = torch.tensor([[SOS_token]], device=device)
+    decoder_hidden = encoder_hidden
+
+    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+
+    if use_teacher_forcing:
+        # Teacher forcing: Feed the target as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_hidden, encoder_outputs)
+            loss += criterion(decoder_output, target_tensor[di])
+            decoder_input = target_tensor[di]  # Teacher forcing
+    else:
+        # Without teacher forcing: use its own predictions as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            topv, topi = decoder_output.topk(1)
+            decoder_input = topi.squeeze().detach()  # detach from history as input
+
+            loss += criterion(decoder_output, target_tensor[di])
+            if decoder_input.item() == EOS_token:
+                break
+
+    loss.backward()
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss.item() / target_length
 
 
-def showPlot(points):
-    plt.figure()
-    x = np.arange(len(points))
-    plt.plot(x, points)
-    plt.show()
-
-
-def train(encoder, decoder, total_epoch, use_attn):
-    print("ENCODER INFORMATION:")
-    print(encoder)
-    print()
-    print("DECODER INFORMATION:")
-    print(decoder)
-    print()
-    print("TOTAL EPOCH : " + str(total_epoch))
-    print("USE ATTN : " + str(use_attn))
-    print()
-    print('Start Training...........')
-
-    param = list(encoder.parameters()) + list(decoder.parameters())
-    optimizer = optim.Adam(param, lr=1e-3)
-    criterion = nn.NLLLoss()
+def trainIters(encoder, decoder, n_iters):
+    start = time.time()
+    learning_rate = 0.01
+    plot_every = 100
+    print_every = 100
     plot_losses = []
+    print_loss_total = 0  # Reset every print_every
+    plot_loss_total = 0  # Reset every plot_every
 
-    for epoch in range(total_epoch):
-        since = time.time()
-        running_loss = 0
-        print_loss_total = 0
-        total_loss = 0
+    encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
+    training_pairs = [tensorsFromPair(input_lang, output_lang,random.choice(pairs))for i in range(n_iters)]
+    criterion = nn.NLLLoss()
 
-        for i, data in enumerate(lang_dataloader):
-            in_lang, out_lang = data
-            print(in_lang)
+    for iter in range(1, n_iters + 1):
+        training_pair = training_pairs[iter - 1]
+        input_tensor = training_pair[0]
+        target_tensor = training_pair[1]
 
-            if torch.cuda.is_available():
-                in_lang = in_lang.cuda()
-                out_lang = out_lang.cuda()
+        loss = train(input_tensor, target_tensor, encoder,decoder, encoder_optimizer, decoder_optimizer, criterion)
+        print_loss_total += loss
+        plot_loss_total += loss
 
-            in_lang = Variable(in_lang)
-            out_lang = Variable(out_lang)
-            encoder_outputs = Variable(torch.zeros(MAX_LENGTH, encoder.hidden_size))
+        if iter % print_every == 0:
+            print_loss_avg = print_loss_total / print_every
+            print_loss_total = 0
+            print(print_loss_avg)
+            print('%s (%d %d%%) %.4f' % (timeSince(start, iter / n_iters),iter, iter / n_iters * 100, print_loss_avg))
 
-            if torch.cuda.is_available():
-                encoder_outputs = encoder_outputs.cuda()
-
-            encoder_hidden = encoder.initHidden()
-
-            for ei in range(in_lang.size(1)):
-                encoder_output, encoder_hidden = encoder(in_lang[:, ei], encoder_hidden)
-                encoder_outputs[ei] = encoder_output[0][0]
-
-            decoder_input = Variable(torch.LongTensor([[SOS_token]]))
-            if torch.cuda.is_available():
-                decoder_input = decoder_input.cuda()
-
-            decoder_hidden = encoder_hidden
-
-            loss = 0
+        if iter % plot_every == 0:
+            plot_loss_avg = plot_loss_total / plot_every
+            plot_losses.append(plot_loss_avg)
+            plot_loss_total = 0
 
 
-            if use_attn:
-                for di in range(out_lang.size(1)):
-                    decoder_output, decoder_hidden, decoder_attention = attn_decoder(
-                        decoder_input, decoder_hidden, encoder_outputs)
-                    loss += criterion(decoder_output, out_lang[:, di])
-                    topv, topi = decoder_output.data.topk(1)
-                    ni = topi[0][0]
 
-                    decoder_input = Variable(torch.LongTensor([[ni]]))
-                    if torch.cuda.is_available():
-                        decoder_input = decoder_input.cuda()
-                    if ni == EOS_token:
-                        break
-            else:
-                for di in range(out_lang.size(1)):
-                    decoder_output, decoder_hidden = decoder(
-                        decoder_input, decoder_hidden)
-                    loss += criterion(decoder_output, out_lang[:, di])
-                    topv, topi = decoder_output.data.topk(1)
-                    ni = topi[0][0]
-
-                    decoder_input = Variable(torch.LongTensor([[ni]]))
-                    if torch.cuda.is_available():
-                        decoder_input = decoder_input.cuda()
-                    if ni == EOS_token:
-                        break
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.data[0]
-            print_loss_total += loss.data[0]
-            total_loss += loss.data[0]
-            if (i + 1) % 5000 == 0:
-                print('{}/{}, Loss:{:.6f}'.format(
-                    i + 1, len(lang_dataloader), running_loss / 5000))
-                running_loss = 0
-            if (i + 1) % 100 == 0:
-                plot_loss = print_loss_total / 100
-                plot_losses.append(plot_loss)
-                print_loss_total = 0
-        during = time.time() - since
-        print('Finish {}/{} , Loss:{:.6f}, Time:{:.0f}s'.format(
-            epoch + 1, total_epoch, total_loss / len(lang_dataset), during))
-        print()
-    showPlot(plot_losses)
-
-
-if use_attn:
-    train(encoder, attn_decoder, total_epoch, use_attn=True)
-else:
-    train(encoder, decoder, total_epoch, use_attn=False)
-
-if use_attn:
-    torch.save(encoder.state_dict(), './encoder.pth')
-    torch.save(attn_decoder.state_dict(), './attn_decoder.pth')
-else:
-    torch.save(encoder.state_dict(), './encoder.pth')
-    torch.save(decoder.state_dict(), './decoder.pth')
-
-
+encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
+attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
+trainIters(encoder1, attn_decoder1, 75000)
