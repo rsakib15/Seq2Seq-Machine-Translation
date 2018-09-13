b2dc9971604976367bf45e68dc1611057a5804a5
Sakib Rahman <srskb15@gmail.com>
2018-09-13 21:10:08 +0600

    SA | Evaluate randomly with sentence

--- evaluate.py
+++ evaluate.py
@@ -1,56 +1,102 @@
+import random
+import time
+import matplotlib.ticker as ticker
 
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
+from torch import nn, optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 
-from dataset import TextDataset,tensorFromSentence
+from dataset import prepareData,tensorFromSentence
 from model.seq2seq import AttnDecoderRNN, DecoderRNN, EncoderRNN
 
 SOS_token = 0
 EOS_token = 1
 MAX_LENGTH = 10
-use_attn = True
-use_cuda = torch.cuda.is_available()
-lang_dataset = TextDataset()
-
-
-lang_dataloader = DataLoader(lang_dataset, shuffle=True)
-input_size = lang_dataset.input_lang_words
 hidden_size = 256
-output_size = lang_dataset.output_lang_words
 total_epoch = 20
+use_attn = True
+teacher_forcing_ratio = 0.5
 
-encoder = EncoderRNN(lang_dataset.input_lang_words, hidden_size, n_layers=2)
-decoder = DecoderRNN(lang_dataset.output_lang_words, output_size, n_layers=2)
-attn_decoder = AttnDecoderRNN(hidden_size, output_size, n_layers=2)
-
-
-def evaluate(sentence, max_length=MAX_LENGTH):
-    input_tensor = tensorFromSentence(lang_dataset.input_lang, sentence)
-    input_length = input_tensor.size()[0]
-    encoder_hidden = encoder.initHidden()
-    encoder_outputs = torch.zeros(max_length, encoder.hidden_size)
-    for ei in range(input_length):
-        encoder_output, encoder_hidden = encoder(input_tensor[ei],encoder_hidden)
-        encoder_outputs[ei] += encoder_output[0, 0]
+input_lang, output_lang, pairs = prepareData('eng', 'ben', False)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
+def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
+    with torch.no_grad():
+        input_tensor = tensorFromSentence(input_lang, sentence)
+
+        input_length = input_tensor.size()[0]
+        encoder_hidden = encoder.initHidden()
+        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+
+        for ei in range(input_length):
+            encoder_output, encoder_hidden = encoder(input_tensor[ei],encoder_hidden)
+            encoder_outputs[ei] += encoder_output[0, 0]
+
+        decoder_input = torch.tensor([[SOS_token]], device=device)  # SOS
+        decoder_hidden = encoder_hidden
+        decoded_words = []
+        decoder_attentions = torch.zeros(max_length, max_length)
+
+
+        for di in range(max_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_hidden, encoder_outputs)
+
+            decoder_attentions[di] = decoder_attention.data
+            topv, topi = decoder_output.data.topk(1)
+            if topi.item() == EOS_token:
+                decoded_words.append('<EOS>')
+                break
+            else:
+                decoded_words.append(output_lang.index2word[topi.item()])
+
+            decoder_input = topi.squeeze().detach()
 
 
 
-    # Run through decoder
-    print(lang_dataset.input_lang.index2word)
-    print(lang_dataset.output_lang.index2word)
-    return decoded_words
+        return decoded_words, decoder_attentions[:di + 1]
 
 
-def main():
-    user_input = 'how are you'
-    sentence = user_input
-    output_words = evaluate(sentence)
-    output_sentence = ' '.join(output_words)
-    print("Sentence: {}\nTranslated Sentence: {}".format(user_input, output_sentence))
+encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
+attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
+
+def evaluateRandomly(encoder, decoder, n=10):
+    for i in range(n):
+        pair = random.choice(pairs)
+        print('>', pair[0])
+        print('=', pair[1])
+        output_words, attentions = evaluate(encoder, decoder, pair[0])
+        output_sentence = ' '.join(output_words)
+        print('<', output_sentence)
+        print('')
 
 
-if __name__ == "__main__":
-    main()
+evaluateRandomly(encoder1, attn_decoder1)
+
+def showAttention(input_sentence, output_words, attentions):
+    # Set up figure with colorbar
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(attentions.numpy(), cmap='bone')
+    fig.colorbar(cax)
+    # Set up axes
+    ax.set_xticklabels([''] + input_sentence.split(' ') +['<EOS>'], rotation=90)
+    ax.set_yticklabels([''] + output_words)
+
+    # Show label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
+
+def evaluateAndShowAttention(input_sentence):
+    output_words, attentions = evaluate(encoder1, attn_decoder1, input_sentence)
+    print('input =', input_sentence)
+    print('output =', ' '.join(output_words))
+    showAttention(input_sentence, output_words, attentions)
+
+
+evaluateAndShowAttention("I told them to send me another ticket.")
